38354f93b25e53f9c2fb8d4f4cf9ecfcb9dbd556
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-04-05 10:17:06 -0400

    correcting login page

--- Public/form.js
+++ Public/form.js
@@ -12,3 +12,24 @@ let dropdown = document.getElementsByClassName('dropdown-content')[0]
 dropbtn.addEventListener('click', () => {
     dropdown.classList.toggle('hide')
 })
+
+var objPeople = [
+    {
+        username:'Sam',
+        password: 'codify'
+    }
+]
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for ( i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password){
+            console.log(username + " is logged in!")
+        } 
+    }
+}
+
+function login() {
+    var username = 
+}
